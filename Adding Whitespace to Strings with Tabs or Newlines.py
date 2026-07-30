d52392e23a5ab616879988cc158ad552cecebd16
MiddleSlime <amirabdulaev2007@gmail.com>
2026-07-31 01:05:48 +0300

    Create Adding Whitespace to Strings with Tabs or Newlines.py
    There are some basics which I missed

---
+++ Adding Whitespace to Strings with Tabs or Newlines.py
@@ -0,0 +1,35 @@
+
+
+
+print("\tAttempt1.0")
+print("\nAttempt1.1 \nAttempt1.2 \nAttempt1.3 \nAttempt1.4")
+# There are \t and \n
+
+Shkibidi_Toilet = " Paramount Pictures "
+print(Shkibidi_Toilet.rstrip())
+print(Shkibidi_Toilet.lstrip())
+print(Shkibidi_Toilet.strip())
+# Also cool thing to correct user input from programme
+
+github = 'https://github.com'
+print(github.removeprefix('https://'))
+
+# 2-3. Personal Message
+name = "Patric"
+print(f"Hello {name}, would you like to learn Python?")
+# 2-4. Name Cases
+print(name.lower())
+print(name.upper())
+print(name.title())
+# 2-5. Famous Quote
+print('Oximiron once said "What if we live in the World full of Shkibidi?"')
+# 2-6. Famous Quote 2
+famous_name = "'Oximiron'"
+print(f'{famous_name} once said "What if we live in the World full of Shkibidi?"')
+# 2-7. Stripping Names
+print(famous_name.rstrip().lstrip())
+print(famous_name.rstrip())
+print(famous_name.lstrip())
+print(famous_name.strip())
+# 2-8. File Extensions
+print(github.removesuffix('com/'))
