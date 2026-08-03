7db7c6777299363a6fa1c58d2f4ce2195520b76b
MiddleSlime <amirabdulaev2007@gmail.com>
2026-08-03 15:50:15 +0300

    Create Lists.py
    Lists and some things

---
+++ Lists.py
@@ -0,0 +1,53 @@
+# There are list and methods how to work with them
+
+
+best_friends = ["MishkaFrede", 'Dohodyaga', 'Gaster' ]
+print(best_friends)
+print(best_friends[0])
+print(best_friends[1].upper())
+print(best_friends[-1])
+
+# 3-1 Names
+print(best_friends[0:])
+print(best_friends[1:])
+print(best_friends[2:])
+# 3-2 Greetings
+print(f'Hello Sigma {best_friends[0]}, how are you?')
+print(f'Hello Sigma {best_friends[1]}, how are you?')
+print(f'Hello Sigma {best_friends[2]}, how are you?')
+# 3-3 Your Own List
+transportation = ['Foot', 'car', 'bus', 'flying']
+print(f'Everyday I use my {transportation[0]} to buy some products!')
+print(f'But sometimes I buy too much and I call a {transportation[1]}')
+print(f'And if i dont have much products I go smw by {transportation[2]}')
+print(f'Who is {transportation[3]} is gay')
+
+# 3-4, 3-5, 3-6, 3-7 Guest List: Guys you are here W W WW
+invite_list = ['Ashhhhh_', 'Redwin', 'PythonNoobie', 'darksoul' ]
+new_invite = invite_list.pop(0)
+print(f'I am really sorry about your anal fissure {new_invite}!')
+invite_list.insert(1, "Shkibidi")
+invite_list.insert(0, "Eblan")
+invite_list.insert(3, "Petux")
+invite_list.append("Mudila")
+print(invite_list)
+
+print(f'Sorry {invite_list.pop(0)} I am not gay anymore')
+print(f'Sorry {invite_list.pop(1)} I am not gay anymore')
+print(f'Sorry {invite_list.pop(2)} I am not gay anymore')
+print(f'Sorry {invite_list.pop(3)} I am not gay anymore')
+print(f'Sorry {invite_list.pop(0)} I am not gay anymore')
+
+print(invite_list)
+print(f'{invite_list} it is all fake we go together')
+del invite_list[0]
+print(invite_list)
+del invite_list[0]
+print(invite_list)
+
+
+
+
+
+
+
