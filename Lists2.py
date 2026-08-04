c1846e6000c8178239d2a22f2c5a4c1e3624f615
MiddleSlime <amirabdulaev2007@gmail.com>
2026-08-04 19:34:08 +0300

    Create Lists2.py
    reverse, sorted, sort and conclusion for the list1

---
+++ Lists2.py
@@ -0,0 +1,27 @@
+
+
+towns = ['New York', 'Toronto', 'Madrid', 'Amnegiya', 'Shahrizabs']
+print(towns)
+print(sorted(towns))
+print(towns)
+towns.sort(reverse=True)
+print(towns)
+print(towns)
+towns.reverse()
+print(towns)
+towns.sort()
+print(towns)
+towns.sort(reverse=True)
+print(towns)
+
+while True:
+    Dnd_Kingdoms = ['Amnekiya', 'Amnegiya', 'Shkibidistan', 'Ubiyvas_bomjegrad']
+    print(Dnd_Kingdoms)
+    print(Dnd_Kingdoms[1])
+    print(f'Dear {Dnd_Kingdoms[0]}, we glad to see how you become a fully independant government')
+    Dnd_Kindomslol = Dnd_Kingdoms[0]
+    print(f'Unfortunately, only {Dnd_Kindomslol} are stay after Red Gay Miasma')
+    del Dnd_Kingdoms[0]
+    print(Dnd_Kingdoms)
+    del Dnd_Kingdoms[0]
+    break
